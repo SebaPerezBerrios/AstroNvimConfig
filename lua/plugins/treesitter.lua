deeb83af867301bdbb1afb7510ea7b3e662b07a5
Sebastian Perez <sebastian.perezberrios@externos-cl.cencosud.com>
2025-05-19 14:11:25 -0400

    update lsp configs

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,23 +1,23 @@
 -- Customize Treesitter
 
 ---@type LazySpec
-return {{
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "lua",
-      "vim",
-      -- add more arguments for adding more treesitter parsers
-    },
-    incremental_selection = {
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "lua",
+        "vim",
+        -- add more arguments for adding more treesitter parsers
+      },
+      incremental_selection = {
         enable = true,
         keymaps = {
           init_selection = "<C-n>",
           node_incremental = "<C-n>",
           node_decremental = "<C-m>",
         },
-      }
+      },
+    },
   },
-},
-  { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
 }
--- lua/plugins/typescript.lua
+++ lua/plugins/typescript.lua
@@ -1,6 +1,5 @@
 return {
   "AstroNvim/astrolsp",
-  optional = true,
   opts = {
     autocmds = {
       eslint_fix_on_save = false,
